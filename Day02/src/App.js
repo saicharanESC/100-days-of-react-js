41ad2e48bc81deb68fc9eb133354cf213b0258c4
saicharanESC <epoorusaicharan@gmail.com>
2025-07-30 22:36:21 +0530

    React Day 2 learning JSX

---
+++ Day02/src/App.js
@@ -0,0 +1,14 @@
+//App.js
+
+import Welcome from "./Welcome";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World, React</h1>
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
