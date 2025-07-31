9fd8a39b5f40e69b0e82e4255800e0cd3add60ef
saicharanESC <epoorusaicharan@gmail.com>
2025-07-31 22:19:27 +0530

    Day 3 - Props and dynamic components

---
+++ Day03/src/App.js
@@ -0,0 +1,14 @@
+import Greetings from './Greeting';
+
+function App() {
+  return (
+    <div>
+      <h1>Day 3: Props in React</h1>
+      <Greetings name="Saicharan" />
+      <Greetings name="React Learner" />
+      <Greetings name="World" />
+    </div>
+  );
+}
+
+export default App;
